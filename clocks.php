9630c0226014b7db56397d2c76555a9e3716d3f9
Unknown <danilin2010@yandex.ru>
2018-01-23 02:28:35 +0300

    Удаление лишнего из /clocks.php

--- clocks.php
+++ clocks.php
@@ -5,17 +5,6 @@ $APPLICATION->SetTitle("Вывод времени");
 	"testtandoors:showtime", 
 	".default", 
 	array(
-		"CACHE_TIME" => "3600",
-		"CACHE_TYPE" => "A",
-		"COUNT" => "0",
-		"IBLOCK_CODE" => "",
-		"IBLOCK_ID" => "0",
-		"IBLOCK_TYPE" => "-",
-		"SHOW_NAV" => "N",
-		"SORT_DIRECTION1" => "ASC",
-		"SORT_DIRECTION2" => "ASC",
-		"SORT_FIELD1" => "ID",
-		"SORT_FIELD2" => "ID",
 		"COMPONENT_TEMPLATE" => ".default",
 		"DATE_FORMAT" => "d.m.Y H:i"
 	),
